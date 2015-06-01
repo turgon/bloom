a083a1ea167f001f1013a1291e754b58d25077f5
Nick S <turgon@gmail.com>
2015-06-01 05:14:27 +0000

    more tests

--- bloom_test.go
+++ bloom_test.go
@@ -6,14 +6,32 @@ import (
 )
 
 func TestNewBloom(t *testing.T) {
-	s := NewBloom(1, 1)
+	s := NewBloom(64, 1)
 	if s.k != 1 {
 		t.Errorf("NewBloom returned wrong hashes for filter")
 	}
-	if s.m != 1 {
+	if s.m != 64 {
 		t.Errorf("NewBloom returned wrong filter size")
 	}
 	if cap(s.collection) != 1 {
 		t.Errorf("NewBloom built wrong sized collection of uint64s")
+	}
+}
+
+func TestJustTest(t *testing.T) {
+	b := NewBloom(128, 1)
+	if b.Test([]byte("test")) {
+		t.Errorf("Insert or Test broken")
+	}
+}
+
+func TestInsertAndTest(t *testing.T) {
+	b := NewBloom(128, 1)
+	b.hasher = func (b *Bloom, value []byte) ([]uint64) {
+		return make([]uint64, 1)
+	}
+	b.Insert([]byte("test"))
+	if !b.Test([]byte("test")) {
+		t.Errorf("Insert or Test broken")
 	}
 }
